e894a288f98908d29aeb0f7c7e4022e94291985e
Matheus Wiethan <80273721+Wietheus@users.noreply.github.com>
2021-10-03 20:57:59 -0300

    Adicionando o exercício de API Rest.

---
+++ API_Rest_Pokemon/Controllers/PokemonController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using API_Rest_Pokemon.Models;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+
+namespace API_Rest_Pokemon.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private static int codigo = 0;
+        private static List<Pokemon> listaPokemon = new List<Pokemon>();
+
+        [HttpPost]
+        public void CadastrarPokemon([FromBody] Pokemon pokemon)
+        {
+            codigo++;
+            pokemon.Codigo = codigo;
+            listaPokemon.Add(pokemon);
+            Console.WriteLine($"[POKÉMON CADASTRADO COM SUCESSO]\n\nCódigo: {pokemon.Codigo}\nNome: {pokemon.Nome}\nTipo: {pokemon.Tipo}\n");
+        }
+
+        [HttpDelete("{codigo}")]
+        public IActionResult RemoverPokemon(int codigo)
+        {
+            foreach (Pokemon pokemon in listaPokemon)
+            {
+                if (pokemon.Codigo == codigo)
+                {
+                    listaPokemon.Remove(pokemon);
+                    Console.WriteLine($"[POKÉMON REMOVIDO COM SUCESSO]\n\nCódigo: {pokemon.Codigo}\nNome: {pokemon.Nome}\nTipo: {pokemon.Tipo}\n");
+                    return Ok(pokemon);
+                }
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        public IEnumerable<Pokemon> VisualizarListaDePokemon()
+        {
+            Console.WriteLine("[EXIBINDO LISTA DE POKÉMON]\n");
+            return listaPokemon;
+        }
+    }
+}
---
+++ API_Rest_Pokemon/Models/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+
+namespace API_Rest_Pokemon.Models
+{
+    public class Pokemon
+    {
+        public int Codigo { get; set; }
+        [Required(ErrorMessage = "Você precisa preencher um nome!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Você precisa informar o tipo!")]
+        public string Tipo { get; set; }
+    }
+}
